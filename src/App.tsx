4f6f53f7426b6b44327193278ce9e228ca934161
Leonard Velásquez <leonard.avp@gmail.com>
2022-10-16 22:51:29 -0400

    Corrección de formula

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,41 @@ import Form from './components/Form'
 
 function App() {
   const [NotaMinima, setNotaMinima] = useState(12)
-  const [{sumatoria,total}, setNotaDefinitiva] = useState({
-    sumatoria:0,
-    total:0
+  const [{ sumatoria, total }, setNotaDefinitiva] = useState({
+    sumatoria: 0,
+    total: 0
   })
+  const [validNotas, setValidNotas] = useState(true)
 
-  const onTipoChange = (e:ChangeEvent<HTMLSelectElement>) => {
-    setNotaMinima( parseInt(e.target.value))
+  const onTipoChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setNotaMinima(parseInt(e.target.value))
   }
 
-  const onNotaChange = (notas: number[]) =>{
-    console.log(notas)
+  const calcNotas = (notas: number[]) => {
+    let sum = 0
+    notas.forEach(nota => sum += nota)
+    return sum
+  }
+
+  const onNotaChange = (notas: number[]) => {
+    const sumatoriaNotas = calcNotas(notas)
+    const notaDef = {
+      sumatoria: sumatoriaNotas,
+      total: 0
+    }
+
+    if(sumatoriaNotas % 5 > 0){
+      notaDef.total = parseInt((sumatoriaNotas / 5).toString().split('.')[0]) + 1
+    } else {
+      notaDef.total = sumatoriaNotas / 5
+    }
+
+    if (notaDef.sumatoria > 100) {
+      setValidNotas(false)
+      return
+    }
+    setValidNotas(true)
+    setNotaDefinitiva(notaDef)
   }
 
   return (
@@ -25,7 +49,7 @@ function App() {
           <div className="col">
             <label className='fs-5 text-center' htmlFor="notaMinima">Tipo de asignatura:</label>
             <select onChange={onTipoChange} className='form-select p-1 my-2' value={NotaMinima} name="notaMinima" id="notaMinima">
-              <option value="12">Regulares</option>
+              <option value="12">Regular</option>
               <option value="16">Proyecto</option>
             </select>
           </div>
@@ -34,16 +58,31 @@ function App() {
           </div>
         </div>
       </div>
-      <Form dispatchNotas={onNotaChange}/>
+      <Form dispatchNotas={onNotaChange} />
       <div className="container mt-4 text-center row">
         {
-          sumatoria <= 100 && 
-          <span className='fs-5'> Sumatoria : {sumatoria} - Total : {total} </span>
+          validNotas &&
+          <>
+            {
+              sumatoria <= 100 &&
+              <span className='fs-5'> Sumatoria : {sumatoria} - Total : {total} </span>
+            }          
+            {
+              total < NotaMinima &&
+              <span className='fs-5 text-danger'>Asignatura aplazada</span>
+            }
+            {
+              total >= NotaMinima &&
+              <span className='fs-5 text-success'>Asignatura aprobada</span>
+            }
+          </>
         }
         {
-          <span className='fs-5'>Asignatura aplazada</span>
+          !validNotas &&
+          <span className='fs-5 text-danger'>Notas no validas</span>
         }
       </div>
+
     </div>
   )
 }
--- src/hooks/useArrayForm.ts
+++ src/hooks/useArrayForm.ts
@@ -1,11 +1,15 @@
 import { useState } from "react"
 
 interface useArrayFormOptions {
-    maxAmountOfValues: number
+    maxAmountOfValues: number,
+    maxValue:number,
+    minValue:number,
 }
 
-const defaultOptions = {
-    mamaxAmountOfValues: 10
+const defaultOptions : useArrayFormOptions = {
+    maxAmountOfValues: 10,
+    maxValue:25,
+    minValue:0
 }
 
 export const useArrayForm = (defaultArrayValue:string[],options = defaultOptions) =>{
@@ -13,14 +17,20 @@ export const useArrayForm = (defaultArrayValue:string[],options = defaultOptions
 
     const onInputChange = (index: number,newValue:string) => {
         const copyOfValues = [...values]
-        copyOfValues[index] = newValue
+        if(parseInt(newValue) > options.maxValue){
+            copyOfValues[index] = options.maxValue.toString()
+        } else if (parseInt(newValue) < options.minValue ) {
+            copyOfValues[index] = options.maxValue.toString()
+        } else {
+            copyOfValues[index] = newValue
+        }
         setValues([...copyOfValues])
     }
 
     const reset = () => setValues(defaultArrayValue)
 
     const addInput = (value = '') => {
-        if(values.length >= options.mamaxAmountOfValues) return
+        if(values.length >= options.maxAmountOfValues) return
         setValues([...values,value])
     } 
 
